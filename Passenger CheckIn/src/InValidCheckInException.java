6452265ea58c7c059e088174a6096dcdf83a4b18
mfais <mfais@192.168.136.1>
2018-02-18 00:09:08 -0800

    Updated the error message

--- Passenger CheckIn/src/InValidCheckInException.java
+++ Passenger CheckIn/src/InValidCheckInException.java
@@ -4,7 +4,7 @@ public class InValidCheckInException  extends Exception{
 	
 	public InValidCheckInException() 
 	{		
-		super("Max Baggage/person < 100 kg & Dimensions < 19x19x19 (LxBxH)");	
+		super("Max Baggage/person <= 100 kg & Dimensions should be <= 20x20x20 (LxBxH)");	
 		
 	}
 }
